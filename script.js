afdab240bcdd247929d9851dc9b3eb8e538425a8
Achyuta Pradhan <achyuta4003@gmail.com>
2022-12-05 17:57:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var typed = new Typed(".typing", {
+        strings: ['Learn HTML', 'Learn CSS', "Learn Javascript", "Learn React js", "Learn Node js"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+})
